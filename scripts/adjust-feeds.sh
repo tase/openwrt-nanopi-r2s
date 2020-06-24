62b65d8bcbd62c9f3b87d36ac112b48faf73952f
Jayantajit Gogoi <jayanta.gogoi525@gmail.com>
2020-06-24 15:21:59 +0000

    fix typo in script

--- scripts/adjust-feeds.sh
+++ scripts/adjust-feeds.sh
@@ -6,5 +6,5 @@ sed -i "s,boardinfo.system,'ARMv8',g" feeds/luci/modules/luci-mod-status/htdocs/
 sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 #irq_optimize
 mkdir package/base-files/files/usr/bin
-wget https://raw.githubusercontent.com/jayanta525/openwrt-nanopi-r2s/circleci/files/irq_optimize.sh package/base-files/files/usr/bin/irq_optimize.sh
-wget https://raw.githubusercontent.com/jayanta525/openwrt-nanopi-r2s/circleci/files/irq_optimize package/base-files/files/etc/init.d/irq_optimize
+wget https://raw.githubusercontent.com/jayanta525/openwrt-nanopi-r2s/circleci/files/irq_optimize.sh -O package/base-files/files/usr/bin/irq_optimize.sh
+wget https://raw.githubusercontent.com/jayanta525/openwrt-nanopi-r2s/circleci/files/irq_optimize -O package/base-files/files/etc/init.d/irq_optimize
